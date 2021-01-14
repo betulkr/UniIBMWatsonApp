b401d4b00087e2b0667b238254eb3821e621cd93
betulkr <58889194+betulkr@users.noreply.github.com>
2021-01-14 14:24:09 +0300

    Son

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace api
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lütfen Analiz için resim seçin";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(57, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(332, 285);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(420, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ANALİZ SONUCU";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(57, 336);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Fotoğraf Yükle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(165, 384);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(420, 71);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Sonuç:";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using IBM.Cloud.SDK.Core.Authentication.Iam;
+using IBM.Cloud.SDK.Core.Http;
+using IBM.Watson.VisualRecognition.v3;
+using IBM.Watson.VisualRecognition.v3.Model;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace api
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Kendi API Key ve Service Url'inizi girebilirsiniz. Deneme amaçlı kendi hesabımdaki değerleri girdim.
+        public void classifyImageFromUrl(string url)
+        {
+            IamAuthenticator authenticator = new IamAuthenticator(
+            apikey: "frpMt8ewuyNbRVm8c4kd9BdiqKSKxGtRvL9BQ_zFQSG5"
+            );
+
+            VisualRecognitionService visualRecognition = new VisualRecognitionService("2018-03-19", authenticator);
+            visualRecognition.SetServiceUrl("https://api.us-south.visual-recognition.watson.cloud.ibm.com/instances/726905b9-bd34-48d0-a9af-71b27f3a31a8");
+
+            var result = visualRecognition.Classify(
+                url: url
+                );
+
+            // Dönen tüm sonuçları consol'da görebilirsiniz.
+            Console.WriteLine(result.Response);
+
+            var img = JObject.Parse(result.Response);
+            //label6.Text = ( img["images"][0]["classifiers"][0]["classes"][0]["class"]).ToString();
+
+        }
+
+        public void classifyImageFromFile(string fileName)
+        {
+            float t=6/10;
+            IamAuthenticator authenticator = new IamAuthenticator(
+            apikey: "BfHmP164OXAf7JERkEwGyVhS_y9UY2TPVdULAhzivREA"
+            );
+
+            VisualRecognitionService visualRecognition = new VisualRecognitionService("2018-03-19", authenticator);
+            visualRecognition.SetServiceUrl("https://api.us-south.visual-recognition.watson.cloud.ibm.com/instances/45b8e4f6-e8a1-4ef7-9510-df2483784053");
+
+            DetailedResponse<ClassifiedImages> result;
+            using (FileStream fs = File.OpenRead(fileName))
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    fs.CopyTo(ms);
+                    result = visualRecognition.Classify(
+                        imagesFile: ms,
+                        imagesFilename: fileName,
+                        threshold: t,
+                        classifierIds: new List<string>()
+                        {
+                            "kalca_844752752"
+                        },
+                        owners: new List<string>()
+                        {
+                            "me"
+                        }
+                        );
+                }
+            }
+
+            
+            var img = JObject.Parse(result.Response);
+            Console.WriteLine(result.Response);
+            // JSON'da birden fazla sonuç getiriyor. Ben ilk gelen sonucu label'a yazdırdım. Ama doğruluk oranına göre de yazdırılabilir. 
+            //label3.Text = ( img["images"][0]["classifiers"][0]["classes"][0]["class"]).ToString();
+            label7.Text=(result.Response);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opnfd = new OpenFileDialog();
+            opnfd.Filter = "Image Files (*.jpg;*.jpeg;.*.gif;)|*.jpg;*.jpeg;.*.gif";
+            if (opnfd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(opnfd.FileName);
+                classifyImageFromFile(opnfd.FileName);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string url = textBox1.Text;
+            //classifyImageFromUrl(url);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
